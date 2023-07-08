a5b2352bab3bb2b3e2c409cb4862f357731d09ad
Kontroller <108008396+konroller60@users.noreply.github.com>
2023-07-08 12:16:00 +0200

    Add files via upload

---
+++ Services/IStaticDataService.cs
@@ -0,0 +1,12 @@
+using CodeBase.StaticData;
+
+namespace CodeBase.Infrastructure.Services
+{
+    public interface IStaticDataService : IService
+
+    {
+    void LoadMonsters();
+    MonsterStaticData ForMonster(MonsterTypeId typeId);
+    LevelStaticData ForLevel(string sceneKey);
+    }
+}
---
+++ Services/PersistentProgress/IPersistentProgressService.cs
@@ -0,0 +1,10 @@
+using CodeBase.Data;
+
+namespace CodeBase.Infrastructure.Services.PersistentProgress
+{
+    public interface IPersistentProgressService : IService
+
+    {
+    PlayerProgress Progress { get; set; }
+    }
+}
